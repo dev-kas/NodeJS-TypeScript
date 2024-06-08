72958c6926ab8f088ae4165a0fe69d2e33f92d64
GlitchieTheDev <krishaang_1808@hotmail.com>
2024-06-08 06:53:42 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Your main code goes here.
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+console.log("Hello, World!");
+console.log(`Edit ${__filename} to see changes.`);
